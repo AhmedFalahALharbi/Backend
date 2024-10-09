8913819950b42319b3c98230c6cb851068124835
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-10-09 14:50:21 +0300

    finish

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3300;
+const PORT = 3330;
 
 app.use(express.json());
 
